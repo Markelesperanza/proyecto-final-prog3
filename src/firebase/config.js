628968ad2e1c2a348b2eb82a15938d5dab90845f
Markelesperanza <mesperanza@udesa.edu.ar>
2024-11-11 17:20:48 -0300

    Intalación de Fire Base

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCl3hPXG1uiIs-b0R4NPYbpHs2R1KONLaI",
+    authDomain: "proyecto-final-prog3.firebaseapp.com",
+    projectId: "proyecto-final-prog3",
+    storageBucket: "proyecto-final-prog3.firebasestorage.app",
+    messagingSenderId: "335824975578",
+    appId: "1:335824975578:web:52f7df4fc812d667ae17e3"
+  };
