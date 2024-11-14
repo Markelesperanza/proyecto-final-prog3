304d4078ca07ae69a1ed06380715cd297ed42f14
maisaccani <msaccani@udesa.edu.ar>
2024-11-14 13:15:09 -0300

    modificacion del app.js

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Register from './src/screens/Register'
 import Login from './src/screens/Login';
+import HomeMenu from './src/componentes/HomeMenu';
+import Home from './src/screens/Home'
 
 const Stack = createNativeStackNavigator();
 
@@ -11,12 +13,18 @@ export default function App() {
   return (
     <NavigationContainer>
 
-     <Stack.Navigator>
-        <Stack.Screen name="Register" component={ Register } />
-        <Stack.Screen name="Login" component={ Login } />
-     </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen options={{
+          headerShown: false,
+        }} name="HomeMenu" component={HomeMenu} />
+        <Stack.Screen options={{
+          headerShown: false,
+        }} name="Home" component={Home} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
 
-   </NavigationContainer>
+    </NavigationContainer>
 
   );
 }
